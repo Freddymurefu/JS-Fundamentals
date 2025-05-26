d3017486c5d873f563fc44497565ff9c1584ce01
Fredrick Murefu <freddymurefu@gmail.com>
2025-05-26 06:52:18 +0300

    Further Edits to the code logic

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,12 @@
 const args = process.argv.slice(2);
 const count = args.length;
 
-if (count === 1) {
+if (count === 0) {
+  console.log("0 argument");
+} else if (count === 1) {
   console.log("1 argument");
+} else if (count >= 3) {
+  console.log("3 arguments");
 } else {
   console.log(`${count} arguments`);
 }
